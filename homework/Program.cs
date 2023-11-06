576132975f41175d2362ffb28345446c0afef9ab
Kate Zubareva <katerina.vinograd@gmail.com>
2023-11-06 20:56:42 +0300

    task 3

--- homework/Program.cs
+++ homework/Program.cs
@@ -28,16 +28,16 @@
 
 // void Sum(int m, int n)
 // {
-//     Console.Write(SumMN(m - 1, n));
+//     Console.Write(Sum2(m - 1, n));
 // }
-// int SumMN(int m, int n)
+// int Sum2(int m, int n)
 // {
 //     if (m == n)
 //         return 0;
 //     else
 //     {
 //         m++;
-//         m = m + SumMN(m, n);
+//         m = m + Sum2(m, n);
 //         return m;
 //     }
 // }
@@ -47,3 +47,33 @@
 // int n = Convert.ToInt32(Console.ReadLine());
 // Sum(m, n);
 
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+// void A(int m, int n)
+// {
+//     Console.Write(Akkerman(m, n)); 
+// }
+
+// int Akkerman(int m, int n)
+// {
+//     if (m == 0)
+//     {
+//         return n + 1;
+//     }
+//     else if (n == 0 && m > 0)
+//     {
+//         return Akkerman(m - 1, 1);
+//     }
+//     else
+//     {
+//         return (Akkerman(m - 1, Akkerman(m, n - 1)));
+//     }
+// }
+
+// Console.Write("Введите число M: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// A(m,n);
